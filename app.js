ab19f92656e98b8abc4460e2f89a91a3a536950e
t-kurihara <takuya.kurihara00@gmail.com>
2018-03-21 23:23:28 +0900

    [change/add] chenge: start server sorce, add: test source.

---
+++ app.js
@@ -0,0 +1,8 @@
+const fastify = require('./server')();
+
+fastify.register(require('./routes/users'), { logLevel: 'debug' });
+
+fastify.listen(3000, err => {
+  fastify.log.info('fastify listend 3000');
+  if (err) throw err;
+});
---
+++ test/server.test.js
@@ -0,0 +1,2 @@
+'use strict';
+const fastify = require('../server');
